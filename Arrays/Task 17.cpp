7f5d975dd4a042bd5c718c840abccbe8d2920e6d
darinadailova <74071747+darinadailova@users.noreply.github.com>
2020-12-05 18:45:46 +0200

    Add files via upload

---
+++ Arrays/Task 17.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+
+	vector<double>vec;
+	int N;
+	cin >> N;
+	double input;
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		vec.push_back(input);
+	}
+	bool flag = false;
+	for (int i = 0; i < vec.size() / 2; i++) {
+		if (vec[i] == vec[vec.size() - i - 1]) {
+			flag = true;
+		}
+		else {
+			flag = false;
+		}
+	}
+	if (flag) {
+		cout << "yes" << endl;
+	}
+	else {
+		cout << "no" << endl;
+	}
+
+	return 0;
+}
