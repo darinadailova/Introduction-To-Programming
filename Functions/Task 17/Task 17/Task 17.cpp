6429ba4550ca7cbba88c1956b2c35e87264aed85
darinadailova <darinadailova@gmail.com>
2020-12-08 21:00:16 +0200

    added solution for task 17

---
+++ Functions/Task 17/Task 17/Task 17.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void sortArray(int arr[], int N) {
+	int temp;
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			if (arr[i] > arr[j]) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		cout << arr[i] << " ";
+	}
+}
+
+int main() {
+
+	int arr[50];
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sortArray(arr, N);
+
+	return 0;
+}
