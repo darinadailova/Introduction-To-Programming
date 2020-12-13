018e75c0bdd31a913c572425afcdefd9fcab84db
darinadailova <darinadailova@gmail.com>
2020-12-13 18:30:32 +0200

    Create Task 9.cpp

---
+++ Recursion/Task 1/Task 9/Task 9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int numberOfDigits(int number) {
+    int count = 0;
+    if (number <= 0) {
+        return 0;
+    }
+    return 1 + numberOfDigits(number / 10);
+}
+
+int main()
+{
+    unsigned int number;
+    cin >> number;
+    cout << numberOfDigits(number) << endl;
+    
+    return 0;
+}
