9286e7c5c5b6eb1abc441846bf4ccdd5c7593965
darinadailova <darinadailova@gmail.com>
2020-12-13 18:22:07 +0200

    Update Task 10.cpp

--- Recursion/Task 1/Task 10/Task 10.cpp
+++ Recursion/Task 1/Task 10/Task 10.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
-//Да се напише рекурсивна функция, която установява, дали в записа на естественото число n се 
-//съдържа цифрата k.
 
 bool containsDigit(int n, int k) {
     if (n <= 0) {
-        return false;//123
+        return false;
     }
     if (n % 10 == k) {
         return true;
