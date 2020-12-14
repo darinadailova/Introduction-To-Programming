aa39961827eb4ccf42bf0a5ec3e5e86d58228e5c
darinadailova <darinadailova@gmail.com>
2020-12-14 19:16:28 +0200

    Revert "Delete Task 9.cpp"
    This reverts commit 7908147d7543b239830fe47def06cf8edac86c46.

---
+++ Recursion/Task 1/Task 9/Task 9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int numberOfDigits(int number) {
+    if (number <= 0) {
+        return 0;
+    }
+    return 1 + numberOfDigits(number / 10);
+}
+
+int main()
+{
+    unsigned int number;
+    cin >> number;
+    cout << numberOfDigits(number) << endl;
+    
+    return 0;
+}
