448f3c95a011f0426010a2838e716789b6b430ad
darinadailova <darinadailova@gmail.com>
2021-02-01 19:29:07 +0200

    Create Task3.cpp

---
+++ Tasks from past exams/Task3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void printRectangle(int N, int M, char toPrint) {
+    const int NMAX = 25;
+    const int MMAX = 80;
+
+    for(int i = 0; i < (NMAX - N) / 2; i++) {
+        std::cout << '\n';
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 1; j < (MMAX - M) / 2; j++) {
+            std::cout << ' ';
+        }
+
+        for(int j = 0; j < M; j++) {
+            std::cout << toPrint;
+        }
+        std::cout << '\n';
+    }
+
+    for(int i = 0; i < (NMAX - N) / 2; i++) {
+        std::cout << '\n';
+    }
+}
+
+int main() {
+    int N = 5;
+    int M = 8;
+    char toPrint = '*';
+    printRectangle(N, M, toPrint);
+
+    return 0;
+}
