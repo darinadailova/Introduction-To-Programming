ad09f0012f23dea0c4cd3cd1256975e7288cb7be
darinadailova <darinadailova@gmail.com>
2021-02-03 16:29:03 +0200

    Add

---
+++ Tasks from past exams/Task14.cpp
@@ -0,0 +1,52 @@
+//14.	Да се напише функция на C++, със сигнатура int DigitPos(long num, int k), която връща като резултат k-тата цифра на числото
+//num или -1, ако такава не съществува. Брои се от ляво на дясно и се започва от 1.
+#include <iostream>
+
+void revertNum(long&);
+int sizeOfNum(long);
+int DigitPos(long num, int);
+
+int main() {
+
+    std::cout << DigitPos(253664, 5) << '\n';
+
+    return 0;
+}
+
+void revertNum(long& num) {
+    long reversedNumber = 0;
+    int remainder;
+    while(num != 0) {
+        remainder = num % 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+        num /= 10;
+    }
+    num = reversedNumber;
+}
+
+int sizeOfNum(long num) {
+    int count = 0;
+    while(num != 0) {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
+
+int DigitPos(long num, int k) {//5844239 -> 8  9324485
+    int sizeOfNumber = sizeOfNum(num);
+    if(sizeOfNumber < k) {
+        return -1;
+    }
+    
+    revertNum(num);
+    int count = 0;
+    while(num != 0) {
+        int remainder = num % 10;
+        num /= 10;
+        count++;
+        if(count == k) {
+            return remainder;
+        }
+    }
+}
---
+++ Tasks from past exams/Task15.cpp
@@ -0,0 +1,45 @@
+//15.	Да се напише функция на C++, със сигнатура bool doExist(char* symbols, char* word), която проверява дали думата word, се среща в
+//символният низ symbols. Да не се използва вградената библиотека string.
+#include <iostream>
+
+int stringLength(char*);
+bool doExist(char*, char*);
+
+int main() {
+    char a[] = "hellothere";
+    char b[] = "here";
+    std::cout << doExist(a, b) << '\n';
+    return 0;
+}
+
+int stringLength(char* symbols) {
+    int count = 0;
+    for (int i = 0; symbols[i] != '\0'; i++) {
+        count++;
+    }
+    return count;
+}
+
+bool doExist(char* symbols, char* word) {
+    int sizeSymbols = stringLength(symbols);
+    int sizeWord = stringLength(word);
+
+    for (int i = 0; i < sizeSymbols; i++) {
+        if (symbols[i] == word[0]) {
+            int count = 1;
+            for (int j = 1, k = i + 1; j < sizeWord; j++, k++) {
+                if (symbols[k] == word[j]) {
+                    count++;
+                }
+            }
+
+            if (count == sizeWord) {
+                return true;
+            }
+            else {
+                count = 0;
+            }
+        }
+    }
+    return false;
+}
